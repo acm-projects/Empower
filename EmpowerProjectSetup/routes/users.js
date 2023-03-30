0f0aa832ae762d9763be57d7aa590cfb95309cc7
JoshuaM195 <joshuamathew195@gmail.com>
2023-03-29 20:39:27 -0500

    bcrypt

--- EmpowerProjectSetup/routes/users.js
+++ EmpowerProjectSetup/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+module.exports = router
 
 // Getting all
 router.get('/', async (req, res) => {
@@ -17,11 +19,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', getUser, (req, res) => {
     res.json(res.user)
 })
-// Creating one
+
+//create the login router
+
+
+// Creating one (/add)
+
+
 router.post('/', async (req, res) => {
     const user = new User({
         name: req.body.name,
-        email: req.body.email
+        email: req.body.email,
+        password: req.body.password
     })
     try{
         const newUser = await user.save()
@@ -30,6 +39,8 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message})
     }
 })
+
+
 // Updating one
 router.patch('/:id', getUser, async (req, res) => {
     if (req.body.name != null) {
