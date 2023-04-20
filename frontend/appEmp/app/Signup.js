24b7eb012400f8aad9e824c4d5dda5395eee1c4f
JoshuaM195 <joshuamathew195@gmail.com>
2023-04-20 17:28:42 -0500

    redisgn

--- frontend/appEmp/app/Signup.js
+++ frontend/appEmp/app/Signup.js
@@ -1,68 +1,89 @@
-import { StyleSheet, Text, View, TextInput, Button, ImageBackground, Pressable} from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
-import React, {useState} from 'react';
-import {useRouter, useSearchParams, useNavigation} from 'expo-router'
-import { globalElements } from '../ui/globalUI.js';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { useRouter, useSearchParams, useNavigation } from "expo-router";
+import { globalElements } from "../ui/globalUI.js";
 
 const handleSubmit = (u, p) => {
   const usernameRegex = /^[a-zA-Z0-9]{5,11}$/;
   const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\w\s]).{7,}$/;
-  
+
   if (usernameRegex.test(u) && passwordRegex.test(p)) {
-    ()=>{
-      router.push(`./Home?name=${u}`)
+    () => {
+      router.push(`./Home?name=${u}`);
     };
-    console.log('Valid username and password');
+    console.log("Valid username and password");
   } else {
     // Invalid username or password
-    console.log('Invalid username or password');
+    console.log("Invalid username or password");
   }
 };
 
 const Signup = () => {
   const router = useRouter();
-  const {name} = useSearchParams();
+  const { name } = useSearchParams();
   const navigation = useNavigation();
-  const [username, setUsername] = useState('Username');
-  const [password, setPassword] = useState('Password');
+  const [username, setUsername] = useState("Username");
+  const [password, setPassword] = useState("Password");
   return (
     //<LinearGradient color={[globalStyle.strongGradient,globalStyle.weakGradient]}>
     <LinearGradient
-      colors={[globalElements.index0, globalElements.index1, globalElements.index2]}
+      colors={[
+        globalElements.index0,
+        globalElements.index1,
+        globalElements.index2,
+      ]}
       style={loginPage.container}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 0 }}
     >
-    <ImageBackground source={require('../assets/logo.png')} resizeMode="contain">
-      <View style={loginPage.main}>
-        <View style={{style: 'inline'}}>
-      </View>
-          <View style={{padding: 50}}>
-            <TextInput style={loginPage.input} 
-                  placeholder='Username' 
-                  onChangeText={(val) => setUsername(val)}
-                  autoCapitalize="none"
-              />
-              <TextInput style={loginPage.input} 
-                  placeholder='Password'
-                  onChangeText={(val) => setPassword(val)}
-                  autoCapitalize="none"
-                  secureTextEntry
-              />
+      <ImageBackground
+        source={require("../assets/logo.png")}
+        resizeMode="contain"
+      >
+        <View style={loginPage.main}>
+          <View style={{ style: "inline" }}></View>
+          <View style={{ padding: 50 }}>
+            <TextInput
+              style={loginPage.input}
+              placeholder="Username"
+              onChangeText={(val) => setUsername(val)}
+              autoCapitalize="none"
+            />
+            <TextInput
+              style={loginPage.input}
+              placeholder="Password"
+              onChangeText={(val) => setPassword(val)}
+              autoCapitalize="none"
+              secureTextEntry
+            />
           </View>
-          <View style={{alignContent: 'center', alignSelf: 'center', justifyContent: 'row'}}>
-          
-          <Button
-            onPress={ ()=>router.push(`./Home?name=${username}`) }
-            title="Signup"
-            color='black'
-          />
+          <View
+            style={{
+              alignContent: "center",
+              alignSelf: "center",
+              justifyContent: "row",
+            }}
+          >
+            <Button
+              onPress={() => router.push(`./Home?name=${username}`)}
+              title="Signup"
+              color="black"
+            />
           </View>
-      </View>
-    </ImageBackground>
+        </View>
+      </ImageBackground>
     </LinearGradient>
   );
-}
+};
 
 const loginPage = StyleSheet.create({
   container: {
@@ -71,9 +92,9 @@ const loginPage = StyleSheet.create({
   },
   main: {
     maxWidth: 960,
-    alignContent: 'center',
-    alignSelf: 'center',
-    paddingTop: 100
+    alignContent: "center",
+    alignSelf: "center",
+    paddingTop: 100,
   },
   title: {
     fontSize: 64,
@@ -84,16 +105,15 @@ const loginPage = StyleSheet.create({
     color: "#38434D",
   },
   input: {
-    alignSelf: 'center',
-      borderColor: '2 #fff',
-      borderWidth: 2,
-      padding: 12,
-      margin: 10,
-      width: 350,
-      backgroundColor: 'white',
-      borderRadius: 30
+    alignSelf: "center",
+    borderColor: "2 #fff",
+    borderWidth: 2,
+    padding: 12,
+    margin: 10,
+    width: 350,
+    backgroundColor: "white",
+    borderRadius: 30,
   },
-  
 });
 
 export default Signup;
