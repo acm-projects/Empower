30abb29309b73d4904073bc440c0af2d3c2f93c1
JoshuaM195 <joshuamathew195@gmail.com>
2023-04-24 23:39:51 -0500

    signIn page fixed

--- frontend/appEmp/app/SignIn.js
+++ frontend/appEmp/app/SignIn.js
@@ -5,7 +5,14 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  Image,
+  ImageBackground,
 } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+import nightSky from "../assets/nightSky.jpeg";
+import logo from "../assets/logo-removebg-preview.png";
 
 const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState("");
@@ -18,42 +25,60 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>My App</Text>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          placeholder="Username"
-          placeholderTextColor="#003f5c"
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          placeholder="Password"
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.loginBtn}
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
+      <ImageBackground
+        source={nightSky}
+        resizeMode="cover"
+        style={styles.image}
       >
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.loginBtn}
-        onPress={() => {
-          navigation.navigate("SignUp");
-        }}
-      >
-        <Text style={styles.loginText}>SIGN UP</Text>
-      </TouchableOpacity>
+        <View style={styles.overlay} />
+        <Image source={logo} style={styles.logo} />
+      </ImageBackground>
+      <View style={styles.bottomContainer}>
+        <LinearGradient
+          colors={["#FFC0CB", "#4c00b0"]}
+          style={styles.bottomContainer}
+        >
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.inputText}
+              placeholder="Username"
+              placeholderTextColor="#003f5c"
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.inputText}
+              placeholder="Password"
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.loginBtn}
+            onPress={() => {
+              navigation.navigate("Home");
+            }}
+          >
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+          <View style={styles.signupContainer}>
+            <Text style={styles.signupText}>Don't have an account? </Text>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("SignUp");
+              }}
+            >
+              <Text style={[styles.signupText, { color: "#FFC0CB" }]}>
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
@@ -61,41 +86,79 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#000",
+    borderWidth: 0,
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    borderBottomLeftRadius: -40,
+    borderBottomRightRadius: -40,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,.3)",
   },
   logo: {
-    fontWeight: "bold",
-    fontSize: 50,
-    color: "#fb5b5a",
+    width: 200,
+    height: 200,
+    alignSelf: "center",
     marginBottom: 40,
   },
+  bottomContainer: {
+    flex: 1,
+    backgroundColor: "#4c00b0",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 0,
+    borderWidth: 0,
+    overflow: "hidden",
+  },
   inputView: {
-    width: "80%",
+    width: "70%",
     backgroundColor: "#fff",
     borderRadius: 25,
     height: 50,
     marginBottom: 20,
     justifyContent: "center",
     padding: 20,
+    marginTop: 30,
+    marginLeft: 60,
   },
   inputText: {
     height: 50,
     color: "black",
   },
   loginBtn: {
-    width: "80%",
-    backgroundColor: "#fb5b5a",
+    width: "70%",
+    backgroundColor: "#4c00b0",
     borderRadius: 25,
     height: 50,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 40,
+    marginTop: 20,
     marginBottom: 10,
+    marginLeft: 60,
   },
   loginText: {
     color: "white",
+  },
+  gradientContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 0,
+  },
+  signupContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  signupText: {
+    color: "#FFFF",
   },
 });
 
